4e8652901c9872169a0defa96eb39b65c5531cbf
thuytiendang206 <thuytiendang206@users.noreply.github.com>
2018-03-15 14:36:28 +0700

    Enhance UI in header and footer (#7)

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
 import { Component } from 'react';
+import './Footer.scss';
 
 export class Footer extends Component {
   render() {
     return (
       <footer className="app-footer">
-        <span>Copyright 2018 &copy; <a href="https://www.mgm-tp.com/">mgm</a> . All rights reversed.</span>
-        <span className="ml-auto">Powered by <a href="https://www.mgm-tp.com/">mgm</a></span>
+        <div className="container">
+            <span className="float-left">Copyright 2018 &copy; <span>Team-Radio</span> . All rights reversed.</span>
+            <span className="ml-auto float-right">Powered by <a href="https://www.mgm-tp.com/">mgm technology partners Vietnam</a></span>
+        </div>
       </footer>
     )
   }
