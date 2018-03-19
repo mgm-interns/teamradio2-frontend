713c75df34d9a26396b1e310485099277fae9a63
Nam <namhoang.td09@gmail.com>
2018-03-19 15:37:27 +0700

    Add form fields

--- src/Modules/User/Components/Register/RegisterForm.tsx
+++ src/Modules/User/Components/Register/RegisterForm.tsx
@@ -9,6 +9,12 @@ export class RegisterForm extends Component {
           <div className="input-group-prepend">
             <span className="input-group-text"><i className="icon-user"></i></span>
           </div>
+          <input type="text" className="form-control" placeholder="Display name"/>
+        </div>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text"><i className="icon-user"></i></span>
+          </div>
           <input type="text" className="form-control" placeholder="Username"/>
         </div>
         <div className="input-group mb-3">
@@ -27,7 +33,7 @@ export class RegisterForm extends Component {
           <div className="input-group-prepend">
             <span className="input-group-text"><i className="icon-lock"></i></span>
           </div>
-          <input type="password" className="form-control" placeholder="Repeat password"/>
+          <input type="password" className="form-control" placeholder="Confirm password"/>
         </div>
         <button type="button" className="btn btn-block btn-success">Create Account</button>
       </div>
