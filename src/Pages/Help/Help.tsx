db596119706698e9c6f26148e73ec34c26f806fb
Nhac Tat Nguyen <nguyentatnhac@gmail.com>
2018-03-20 16:47:25 +0700

    Remove redundant code

--- src/Pages/Help/Help.tsx
+++ src/Pages/Help/Help.tsx
@@ -6,11 +6,10 @@ import Resource from './HelpResource';
 import './Help.scss';
 
 export class Help extends Component {
-  coverImg = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYUZbD4KRNnATuFo0ZqOOmAU6ecExSjSLxxzDlnTrHgNQs6bvl';
   render() {
     return (
       <div className="help-container">
-        <img src={this.coverImg} className="help-container_cover-image"/>
+        <img src="img/help-page/cover.jpg" className="help-container_cover-image"/>
         <div className="help-container_content">
           <ReactMarkdown source={Resource.score}/>
         </div>
