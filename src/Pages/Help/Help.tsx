c14de94de15f55016d10fa30b09d3fccdfc27704
Nhac Tat Nguyen <nguyentatnhac@gmail.com>
2018-03-20 15:32:07 +0700

    Add cover photo, fix css style

--- src/Pages/Help/Help.tsx
+++ src/Pages/Help/Help.tsx
@@ -3,13 +3,17 @@ import * as ReactMarkdown from 'react-markdown';
 import { Component } from "react";
 
 import Resource from './HelpResource';
+import './Help.scss';
 
 export class Help extends Component {
+  coverImg = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQYUZbD4KRNnATuFo0ZqOOmAU6ecExSjSLxxzDlnTrHgNQs6bvl';
   render() {
     return (
-      <div className="animated fadeIn">
-        <ReactMarkdown source={Resource.general} />
-        <ReactMarkdown source={Resource.score} />
+      <div className="help-container">
+        <img src={this.coverImg} className="help-container_cover-image"/>
+        <div className="help-container_content">
+          <ReactMarkdown source={Resource.score}/>
+        </div>
       </div>
     )
   }
