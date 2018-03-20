2d04ec4e02aa029d55fcaadabec1a30c0abf59e7
Khanh Ly <baokhanh7m@hotmail.com>
2018-03-20 18:24:07 +0700

    Fix error rebase

--- src/Containers/NoSideBarLayout/NoSideBarLayout.tsx
+++ src/Containers/NoSideBarLayout/NoSideBarLayout.tsx
@@ -2,33 +2,42 @@ import * as React from 'react';
 import { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { CustomHeader, Footer } from '../../Components/';
-import { Home, Login, ForgotPassword, Register, ResetPassword, Help } from '../../Pages/';
+import {
+  Home,
+  Login,
+  ForgotPassword,
+  Register,
+  ResetPassword,
+  Help,
+} from '../../Pages/';
 
 export class NoSideBarLayout extends Component {
   render() {
     return (
       <div className="app no-side-bar">
-        <CustomHeader/>
+        <CustomHeader />
         <div className="app sidebar-hidden">
           <main className="main">
-<<<<<<< HEAD
             <Switch>
-              <Route exact path="/" name="Home" component={Home}/>
-              <Route path="/login" name="Login" component={Login}/>
-              <Route path="/register" name="Register" component={Register}/>
-              <Route path="/forgot-password" name="ForgotPassword" component={ForgotPassword}/>
-              <Route path="/reset-password" name="ResetPassword" component={ResetPassword}/>
-              <Route path="/help" name="Help" component={Help}/>
-              <Redirect from="/" to="/"/>
+              <Route exact path="/" name="Home" component={Home} />
+              <Route path="/login" name="Login" component={Login} />
+              <Route path="/register" name="Register" component={Register} />
+              <Route
+                path="/forgot-password"
+                name="ForgotPassword"
+                component={ForgotPassword}
+              />
+              <Route
+                path="/reset-password"
+                name="ResetPassword"
+                component={ResetPassword}
+              />
+              <Route path="/help" name="Help" component={Help} />
+              <Redirect from="/" to="/" />
             </Switch>
-=======
-              <Switch>
-                <Route path="/" name="Home" component={Home}/>
-              </Switch>
->>>>>>> Migrate UI home-page and Add new animation
           </main>
         </div>
-        <Footer/>
+        <Footer />
       </div>
     );
   }
