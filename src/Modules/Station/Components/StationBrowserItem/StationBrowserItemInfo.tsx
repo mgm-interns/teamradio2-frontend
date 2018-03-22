08fbb61e4e00887c3c8eafe90962cdf83b6b527e
unknown <bao.mmo3@gmail.com>
2018-03-22 13:54:39 +0700

    Update scroll bar

--- src/Modules/Station/Components/StationBrowserItem/StationBrowserItemInfo.tsx
+++ src/Modules/Station/Components/StationBrowserItem/StationBrowserItemInfo.tsx
@@ -2,10 +2,14 @@ import { Component } from 'react';
 import * as React from 'react';
 import './StationBrowserItem.scss'
 
-export class StationBrowserItemInfor extends Component {
+export class StationBrowserItemInfo extends Component {
   render() {
     return (
       <div className="station-thumbnail">
+        <img src="https://www.w3schools.com/images/picture.jpg"/>
+        <div className="online-user">
+          <span> 0 online</span>
+        </div>
       </div>
     );
   }
