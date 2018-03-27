5177f70c59d7e2abd13003826864cdc5c0a686b9
Nam <namhoang.td09@gmail.com>
2018-03-27 16:29:05 +0700

    Add mute function

--- src/Modules/Station/Components/PreviewVideo/PreviewVideo.tsx
+++ src/Modules/Station/Components/PreviewVideo/PreviewVideo.tsx
@@ -9,6 +9,10 @@ import './PreviewVideo.scss';
 export class PreviewVideo extends Component<any, any> {
   constructor(props: any) {
     super(props);
+    this.state = {
+      muted: true
+    };
+    this.mutePreview = this.mutePreview.bind(this);
   }
 
   getTitle(video: any) {
@@ -19,15 +23,22 @@ export class PreviewVideo extends Component<any, any> {
     return convertTime(video.contentDetails.duration);
   }
 
+  mutePreview() {
+    this.setState((prevState: any) => ({
+      muted: !prevState.muted
+    }));
+  }
+
   render() {
     const {video} = this.props;
+    const {muted} = this.state;
 
     return (
       <div className="preview">
         {video ? (
           <Row>
             <Col sm="4" xs="12">
-              <ReactPlayer url={YoutubeHelper.getVideoUrl(video)} playing width='100%' height='100%'/>
+              <ReactPlayer url={YoutubeHelper.getVideoUrl(video)} playing width='100%' height='100%' volume={1} muted={muted}/>
             </Col>
             <Col sm="8" xs="12">
               <p>{this.getTitle(video)}</p>
@@ -35,7 +46,16 @@ export class PreviewVideo extends Component<any, any> {
                 <i className="fa fa-clock-o"/>
                 <span className="preview__duration">{this.getDuration(video)}</span>
                 <Input className="preview__input" placeholder="Do you want to say something about this video?"/>
-                <Button className="preview__button" color="primary">Add</Button>
+                <div className="preview__buttons">
+                  {
+                    muted ? (
+                      <i className="icon-volume-off icons font-2xl" onClick={this.mutePreview}/>
+                    ) : (
+                      <i className="icon-volume-2 icons font-2xl" onClick={this.mutePreview}/>
+                    )
+                  }
+                  <Button className="preview__button" color="primary">Add</Button>
+                </div>
               </div>
             </Col>
           </Row>
