332b7c25b70a9822ccf704d22f1b05879baa30e5
Dung Le <dungle1811@gmail.com>
2018-03-27 19:22:03 +0700

    Fix Register form UI

--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -9,29 +9,35 @@ export class Register extends Component {
 
   render() {
     return (
-      <div className="register a app flex-row align-items-center">
-        <div className="background-image"/>
-        <Container>
-          <Col md="12">
-            <div className="card-group">
-              <div className="card text-white py-6 d-md-down-none card-extra-information">
-                <div className="card-container card-body text-center">
-                  <ExtraInformationForm/>
+      //   a app flex-row align-items-center
+      <div className="register">
+        <div className="background-image">
+            <img src="img/register-background.jpg" alt="" className="background-image"/>
+        </div>
+        <div className="main-form-container">
+            <Col md="10" className="main-form-layout">
+                <div className="card-group">
+                    <div className="card text-white py-6 d-md-down-none card-extra-information">
+                        <div className="card-container card-body text-center">
+                            <ExtraInformationForm/>
+                        </div>
+                    </div>
+                    <div className="card p-4">
+                        <h1>Sign Up</h1>
+                        <p className="text-muted">to get the most out of Team Radio</p>
+                        <RegisterForm/>
+                        <Row>
+                            <Col className="login-callout" xs="12" sm="12">
+                                <span>Already have an account? <CardLink href="/login">Login</CardLink></span>
+                            </Col>
+                        </Row>
+                    </div>
                 </div>
-              </div>
-              <div className="card p-4">
-                <h1>Sign Up</h1>
-                <p className="text-muted">to get the most out of Team Radio</p>
-                <RegisterForm/>
-                <Row>
-                  <Col className="login-callout" xs="12" sm="12">
-                    <span>Already have an account? <CardLink href="/login">Login</CardLink></span>
-                  </Col>
-                </Row>
-              </div>
-            </div>
-          </Col>
-        </Container>
+            </Col>
+        </div>
+        {/*<Container>*/}
+          {/**/}
+        {/*</Container>*/}
       </div>
     );
   }
