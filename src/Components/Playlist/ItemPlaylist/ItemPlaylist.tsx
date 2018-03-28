98dcf86de2e61ff84ef2415b79ddf588fc54c1f0
Thuy Tien <Tien.Dang@mgm-tp.com>
2018-03-28 08:47:14 +0700

    Refactor code   - ItemPlaylist, Playlist

--- src/Components/Playlist/ItemPlaylist/ItemPlaylist.tsx
+++ src/Components/Playlist/ItemPlaylist/ItemPlaylist.tsx
@@ -10,9 +10,9 @@ export class ItemPlaylist extends Component<any, any> {
     this.state = {
       isUpVote: false,
       isDownVote: false,
-      upVotes: 0,
-      downVotes: 0,
       isFavourite: false,
+      upVotes: this.props.upVotes,
+      downVotes: this.props.downVotes,
     };
     this.setFavouriteSong = this.setFavouriteSong.bind(this);
     this.setUpVote = this.setUpVote.bind(this);
@@ -26,46 +26,61 @@ export class ItemPlaylist extends Component<any, any> {
 
   setUpVote() {
     this.setState({
-      isUpVote: !this.state.isUpVote
+      isUpVote: !this.state.isUpVote,
+      upVotes: !this.state.isUpVote ? this.state.upVotes + 1 : this.state.upVotes - 1
     })
+
   }
 
   setDownVote() {
     this.setState({
-      isDownVote: !this.state.isDownVote
+      isDownVote: !this.state.isDownVote,
+      downVotes: !this.state.downVotes ? this.state.downVotes + 1 : this.state.downVotes - 1
     })
   }
 
   render(){
-    const isSkip = this.props.isSkip;
-    const mess = this.props.mess ? this.props.mess : undefined;
-    const classes = this.props.isPlaying ? "item-playlist-container isPlaying" : "item-playlist-container";
+    const {
+      song_id,
+      title,
+      isPlaying,
+      thumbnail,
+      creator,
+      duration,
+      willBeSkipped,
+      message,
+    } = this.props;
+    const classes = isPlaying ? "item-playlist-container isPlaying" : "item-playlist-container";
     return(
       <Card className={classes}>
         <Row className="item-row">
           <Col xs="3" className="item-col-video">
-            <img className="item-img" src="https://i.ytimg.com/vi/JGwWNGJdvx8/default.jpg"/>
-            {!isSkip ? (<div className="duration">20:06</div>) : (
-              <div className="skip-icon" id="idSongSkipped"><i className="fa fa-step-forward"></i></div>)}
-            <UncontrolledTooltip placement="bottom"
-                                 target="idSongSkipped">{`'This song will be skipped when player starts it.'`}</UncontrolledTooltip>
+            <img className="item-img" src={thumbnail}/>
+            {!willBeSkipped ? (<div className="duration">{duration}</div>) : (
+              <div>
+                <div className="skip-icon" id={`WillBeSkipped` + song_id}><i className="fa fa-step-forward"></i></div>
+                <UncontrolledTooltip placement="bottom"
+                                     target={`WillBeSkipped` + song_id}>{`'This song will be skipped when player starts it.'`}</UncontrolledTooltip>
+              </div>)}
+
           </Col>
           <Col xs={9} className="item-col-title-vote">
             <Row className="item-row-title-vote">
               <Col xs={10} className="item-col-title">
-                <h5 className="item-title" id="idSong">ABC Song</h5>
+                <h5 className="item-title" id={`Song` + song_id}>{title}</h5>
                 <UncontrolledTooltip placement="bottom"
-                                     target="idSong">{`ABC-Song ^.^`}</UncontrolledTooltip>
+                                     target={`Song` + song_id}>{title}</UncontrolledTooltip>
                 <div className="item-addedBy">
                   <span>Added by</span>
                   <Link to={`/login`}>
-                    <img className="avatar" id="idUserAvatar"
-                         src="https://res.cloudinary.com/cocacode2/image/upload/v1517992692/hhunvpjq7ogvoehk7flk.png"/>
-                    <UncontrolledTooltip placement="bottom" target="idUserAvatar">{`ThuyTien`}</UncontrolledTooltip>
-                    {mess ? (<div>
-                      <i className="icon-speech icons icon-message" id="idMessage"></i>
+                    <img className="avatar" id={"UserAvatar" + song_id}
+                         src={creator.avatar_url}/>
+                    <UncontrolledTooltip placement="bottom"
+                                         target={"UserAvatar" + song_id}>{creator.username}</UncontrolledTooltip>
+                    {message ? (<div>
+                      <i className="icon-speech icons icon-message" id={"Message" + song_id}></i>
                       <UncontrolledTooltip placement="bottom"
-                                           target="idMessage">{mess}</UncontrolledTooltip>
+                                           target={"Message" + song_id}>{message}</UncontrolledTooltip>
                     </div>) : null}
 
                   </Link>
@@ -85,11 +100,11 @@ export class ItemPlaylist extends Component<any, any> {
           {this.state.isUpVote ? (
             <i className="fa fa-thumbs-up thumbs-icon isActive" onClick={() => this.setUpVote()}></i>) : (
             <i className="fa fa-thumbs-up thumbs-icon " onClick={() => this.setUpVote()}></i>)}
-          <span className="padding-right">{2}</span>
+          <span className="padding-right">{this.state.upVotes}</span>
           {this.state.isDownVote ? (
             <i className="fa fa-thumbs-down thumbs-icon isActive" onClick={() => this.setDownVote()}></i>) : (
             <i className="fa fa-thumbs-down thumbs-icon" onClick={() => this.setDownVote()}></i>)}
-          <span>{2}</span>
+          <span>{this.state.downVotes}</span>
         </div>
       </Card>
     )
