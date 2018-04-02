9cae4148386fbc009f6d037b59a4370e2662749d
NguyenAnhQuoc <quocnguyen.it.acoustic1994@gmail.com>
2018-04-02 17:27:18 +0700

    rebase on dev

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,13 +1,11 @@
 import * as React from 'react';
 import { Component } from 'react';
-
+import { StationBrowserForHome } from '../../Modules/Station';
 import { Main } from './Main';
 import { Sections } from './Sections';
-import { StationBrowserForHome } from '../../Modules/Station';
-
 
 export class Home extends Component {
-  render() {
+  public render() {
     return [<Main key={1}/>, <StationBrowserForHome key={2}/>, <Sections key={3}/>];
   }
 }
