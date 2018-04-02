126a920b67567873ce63ec836eabe3cd0187a6cf
mbnguyen <minhnb89@gmail.com>
2018-04-02 16:18:49 +0700

    Revert "Favourite and history layout for Playlist Tabbar (#55)"
    This reverts commit d15fe10

--- src/Modules/Station/Components/Components.tsx
+++ src/Modules/Station/Components/Components.tsx
@@ -2,6 +2,3 @@ export * from './SearchSong';
 export * from './PreviewVideo';
 export * from './NowPlaying';
 export * from './StationSharing';
-export * from './History';
-export * from './Favourite';
-
