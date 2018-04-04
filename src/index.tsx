3877e00f4936efde7095d5582f8a4df4eda85c96
Khanh Ly <baokhanh7m@gmail.com>
2018-04-05 01:39:56 +0700

    Enable simple-line-icón

--- src/index.tsx
+++ src/index.tsx
@@ -8,7 +8,7 @@ import 'flag-icon-css/css/flag-icon.min.css';
 // Import Font Awesome Icons Set
 import 'font-awesome/css/font-awesome.min.css';
 // Import Simple Line Icons Set
-// import 'simple-line-icons/css/simple-line-icons.css';
+import 'simple-line-icons/css/simple-line-icons.css';
 // Temp fix for reactstrap
 import '../scss/core/_dropdown-menu-right.scss';
 // Import Main styles for this application
