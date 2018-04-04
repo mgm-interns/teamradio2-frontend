3b1b28d6aa52a5f87805fe9e2f0bb30d38581cb9
Khanh Ly <baokhanh7m@gmail.com>
2018-04-04 15:47:19 +0700

    Remove redundancy components

--- src/Containers/WithLoadable.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import Loadable from 'react-loadable';
-
-export const WithLoadable = (loader: any) =>
-  Loadable({
-    loader,
-    loading: () => <div>Loading...</div>,
-  });
