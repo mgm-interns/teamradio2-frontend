fec4235bc3701cb72b2df8846922d578aced160d
Hau Tran <hatran@MGM-EDV.DE>
2018-04-05 14:12:44 +0700

    format code of addingSongSection

--- src/Pages/Home/Sections/AddingSongSection.tsx
+++ src/Pages/Home/Sections/AddingSongSection.tsx
@@ -8,25 +8,25 @@ export class AddingSongSection extends Component {
         <div className="container">
           <div className="row flex-column-reverse flex-md-row">
             <div className="col-lg-6 col-lg-push-6">
-                <div className="composition-search">
-                  <img
-                    src="img/home/add-song.gif"
-                    alt="Add Song"
-                    className="composition__photo composition__photo--p4"
-                  />
-                </div>
+              <div className="composition-search">
+                <img
+                  src="img/home/add-song.gif"
+                  alt="Add Song"
+                  className="composition__photo composition__photo--p4"
+                />
+              </div>
             </div>
             <div className="col-lg-6 col-lg-pull-6">
-                <h3 className="heading-tertiary u-margin-bottom-small">
-                  Search & add song to playlist
-                </h3>
-                <p className="paragraph">
-                  Over 1 billion songs on Youtube are embedded to such a tiny
-                  search-box. Live preview helps you choose the right song.
-                </p>
-                <a href="#" className="btn-text">
-                  Easy to search - Easier to add &rarr;
-                </a>
+              <h3 className="heading-tertiary u-margin-bottom-small">
+                Search & add song to playlist
+              </h3>
+              <p className="paragraph">
+                Over 1 billion songs on Youtube are embedded to such a tiny
+                search-box. Live preview helps you choose the right song.
+              </p>
+              <a href="#" className="btn-text">
+                Easy to search - Easier to add &rarr;
+              </a>
             </div>
           </div>
         </div>
