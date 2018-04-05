c7d7b62e08a562301e2f3489240052a1008a368f
Khanh Ly <baokhanh7m@gmail.com>
2018-04-05 17:06:30 +0700

    Solve conflict

--- src/Modules/Station/Components/CreateStation/CreateStation.tsx
+++ src/Modules/Station/Components/CreateStation/CreateStation.tsx
@@ -5,44 +5,37 @@ import { Component } from 'react';
 import { Button, FormFeedback, FormGroup, InputGroup } from 'reactstrap';
 import './CreateStation.scss';
 
-interface IFormValues {
-  stationName: string;
+interface IStationFormValues {
+  name: string;
 }
 
-interface IProps {
-  history?: object;
-}
-
-interface IState {
-  stationName: string;
-  isPrivate: false;
-}
-
-const InnerForm = (props: FormikProps<IFormValues>) => {
+const InnerForm = (props: FormikProps<IStationFormValues>) => {
   const { touched, errors, isSubmitting, handleSubmit } = props;
 
   return (
     <Form onSubmit={handleSubmit} className="form-wrapper">
       <FormGroup className="input-wrapper">
-          <InputGroup>
-            <Field
-              type="text"
-              name="stationName"
-              className="transparent-form-control"
-              placeholder="Your team station"
-            />
-          </InputGroup>
+        <InputGroup>
+          <Field
+            type="text"
+            name="stationName"
+            className="transparent-form-control"
+            placeholder="Your team station"
+          />
+        </InputGroup>
 
-        <Button color="success" block disabled={isSubmitting} className="button-submit">
+        <Button
+          color="success"
+          block
+          disabled={isSubmitting}
+          className="button-submit">
           <i className="fa fa-paper-plane-o input--fa" />
         </Button>
       </FormGroup>
 
-      {touched.stationName &&
-        errors.stationName && (
-          <FormFeedback className="text-error">
-            {errors.stationName}
-          </FormFeedback>
+      {touched.name &&
+        errors.name && (
+          <FormFeedback className="text-error">{errors.name}</FormFeedback>
         )}
     </Form>
   );
@@ -54,46 +47,34 @@ interface IFormProps {
   handleSubmit: any;
 }
 
-const FormWrapper = withFormik<IFormProps, IFormValues>({
+const FormWrapper = withFormik<IFormProps, IStationFormValues>({
   mapPropsToValues: props => {
     return {
-      stationName: props.initialStationName || '',
+      name: props.initialStationName || '',
     };
   },
 
-  validate: (values: IFormValues) => {
+  validate: (values: IStationFormValues) => {
     const errors: FormikErrors<any> = {};
     const { validStationName } = Rules;
-    const stationNameValidator = new Validator('Email', values.stationName, [
+    const stationNameValidator = new Validator('Email', values.name, [
       validStationName,
     ]);
     errors.stationName = stationNameValidator.validate();
     return Validator.removeUndefinedError(errors);
   },
 
-  handleSubmit: (values, { props }) => {
+  handleSubmit: (values: IStationFormValues, { props }) => {
     props.handleSubmit(values);
   },
 })(InnerForm);
 
-export class CreateStation extends Component<IProps, IState> {
-  constructor(props: any) {
-    super(props);
-
-    this.state = {
-      stationName: '',
-      isPrivate: false,
-    };
-
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  public handleSubmit(text: string) {
-    this.setState({stationName: text})
-  }
+export class CreateStation extends Component {
+  public handleSubmit = (formValues: IStationFormValues) => {
+    console.log(formValues);
+  };
 
   public render() {
-    console.log(this.state.stationName);
     return <FormWrapper handleSubmit={this.handleSubmit} />;
   }
 }
