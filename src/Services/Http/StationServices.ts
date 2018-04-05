f8e4aba13aa5ab5b61a8ccd21c9faca106e32f87
Hau Tran <36954907+hau-tran@users.noreply.github.com>
2018-04-05 17:25:49 +0700

    List stations: fetch data from server (#88)

---
+++ src/Services/Http/StationServices.ts
@@ -0,0 +1,15 @@
+import { Station } from 'Models/Station';
+import { Observable } from 'rxjs/Observable';
+import { HttpServices } from './HttpServices';
+
+export class StationServices {
+  private _httpServices: HttpServices;
+
+  constructor() {
+    this._httpServices = new HttpServices();
+  }
+
+  public getListStation(): Observable<Station[]> {
+    return this._httpServices.get('/stations');
+  }
+}
