f67b1fe9f44cc08c9ed2b4896e072a31db73abce
mbnguyen <minhnb89@gmail.com>
2018-04-06 15:39:46 +0700

    Update station services: add more connecting API functions

--- src/Services/Http/StationServices.ts
+++ src/Services/Http/StationServices.ts
@@ -4,12 +4,25 @@ import { HttpServices } from './HttpServices';
 
 export class StationServices {
   private _httpServices: HttpServices;
+  private serviceUrl = 'stations';
 
   constructor() {
     this._httpServices = new HttpServices();
   }
 
   public getListStation(): Observable<Station[]> {
-    return this._httpServices.get('/stations');
+    return this._httpServices.get('stations');
+  }
+
+  public getStationById(id: string): Observable<Station> {
+    return this._httpServices.get(`${this.serviceUrl}/${id}`);
+  }
+
+  public createStation(name: string, privacy?: string): Observable<Station> {
+    const params: any = {
+      name,
+      privacy: privacy || 'station_public'
+    };
+    return this._httpServices.post(this.serviceUrl, params);
   }
 }
