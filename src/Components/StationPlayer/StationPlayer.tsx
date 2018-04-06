b41008f49fcdb061cc823b5310fdca1ca2e73f0f
mbnguyen <minhnb89@gmail.com>
2018-04-06 18:27:21 +0700

    Remove redundant code

--- src/Components/StationPlayer/StationPlayer.tsx
+++ src/Components/StationPlayer/StationPlayer.tsx
@@ -5,8 +5,6 @@ import { Progress } from 'reactstrap';
 
 import './StationPlayer.scss';
 
-const ACCEPTABLE_DELAY = 1;
-
 interface IProps {
   url: string;
   playing: boolean;
@@ -25,11 +23,6 @@ interface IState {
   seekTime: number;
   receivedAt: number;
   isPaused: boolean;
-}
-
-interface ISeekTimeProps {
-  seekTime: number;
-  receivedAt: number;
 }
 
 export class StationPlayer extends Component<IProps, IState> {
@@ -53,7 +46,6 @@ export class StationPlayer extends Component<IProps, IState> {
 
   public render() {
     const { url, playing, showProgressbar, muted }: IProps = this.props;
-    console.log(url);
     return [
       <ReactPlayer
         key={1}
@@ -71,48 +63,5 @@ export class StationPlayer extends Component<IProps, IState> {
       showProgressbar &&
         url && <Progress key={2} className="progress" animated value={10.2} />,
     ];
-  }
-
-  public seekToTime({ seekTime, receivedAt }: ISeekTimeProps) {
-    // console.log(StationPlayer._getExactlySeektime({seekTime, receivedAt}));
-    // this.playerRef.seekTo(StationPlayer._getExactlySeektime({seekTime, receivedAt}));
-  }
-
-  public static _getExactlySeektime({ seekTime, receivedAt }: ISeekTimeProps) {
-    // const currentTime: number = new Date().getTime();
-    // const delayedTime: number = (currentTime - receivedAt) / 1000;
-    // return Math.abs(seekTime + delayedTime);
-  }
-
-  public _onStartPlayer() {
-    // this.seekToTime(this.state);
-  }
-
-  public _onProgressPlayer({ played, loaded, playedSeconds }: any) {
-    // this.setState({
-    //   played: played * 100,
-    //   buffer: loaded * 100,
-    // });
-    // const exactlyTime = StationPlayer._getExactlySeektime(this.state);
-    // const differentTime = Math.abs(exactlyTime - playedSeconds);
-    // if (differentTime > ACCEPTABLE_DELAY) {
-    //   this.playerRef.seekTo(exactlyTime);
-    // }
-  }
-
-  public _onPausePlayer() {
-    // this.setState({
-    //   isPaused: true,
-    // });
-  }
-
-  public _onPlayPlayer() {
-    // if (this.state.isPaused) {
-    //   this.setState({
-    //     isPaused: false,
-    //   });
-    //   const exactlyTime = StationPlayer._getExactlySeektime(this.state);
-    //   this.playerRef.seekTo(exactlyTime);
-    // }
   }
 }
