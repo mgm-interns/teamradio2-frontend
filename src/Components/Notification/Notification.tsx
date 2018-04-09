f42b37458c40197feedd7e97f802fa35e8a2037d
Khanh Ly <baokhanh7m@gmail.com>
2018-04-09 14:32:45 +0700

    Init component with react-notification-system

---
+++ src/Components/Notification/Notification.tsx
@@ -0,0 +1,10 @@
+import * as classNames from 'classnames';
+import * as React from 'react';
+import { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+export class Notification extends Component {
+  public render() {
+    return <div />;
+  }
+}
