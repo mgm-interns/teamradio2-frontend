923e7ad34de5f2bd6ee01464dd4a7ab2c250a205
Tri Pham <pvtri96@gmail.com>
2018-04-12 15:06:23 +0700

    Fix station browser item styling

--- src/Modules/Station/Components/StationBrowser/StationBrowserItem/StationBrowserItem.tsx
+++ src/Modules/Station/Components/StationBrowser/StationBrowserItem/StationBrowserItem.tsx
@@ -1,3 +1,4 @@
+import * as classNames from 'classnames';
 import { Component } from 'react';
 import * as React from 'react';
 import { RouteComponentProps, withRouter } from 'react-router-dom';
@@ -34,21 +35,22 @@ class SBItem extends Component<
       friendlyId,
     } = this.props;
     return (
-      <div className="station-item" onClick={this.joinStation}>
-        <div className="station-thumbnail">
+      <div className="station-item d-flex">
+        <div className="thumbnail">
           <img src={picture || '/img/station_default_cover.png'} />
-          <div className="online-user">
+          <div className="online-users">
             <i
-              className={
-                numberOfOnlineUsers > 0 ? 'fa fa-circle' : 'fa fa-circle-o'
-              }
+              className={classNames('fa', {
+                'fa-circle': numberOfOnlineUsers > 0,
+                'fa-circle-o': numberOfOnlineUsers <= 0,
+              })}
             />
             <span> {numberOfOnlineUsers} online</span>
           </div>
         </div>
         <div className="station-name">
-          <span id={`Station` + friendlyId}>{stationName}</span>
-          <UncontrolledTooltip placement="top" target={`Station` + friendlyId}>
+          <span id={`Station-` + friendlyId}>{stationName}</span>
+          <UncontrolledTooltip placement="top" target={`Station-` + friendlyId}>
             {stationName}
           </UncontrolledTooltip>
         </div>
