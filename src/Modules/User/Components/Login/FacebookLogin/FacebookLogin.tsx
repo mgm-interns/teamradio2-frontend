508bbe82f49c558d996a484db7ae9b3f958a4a34
Khanh Ly <baokhanh7m@gmail.com>
2018-04-16 14:57:15 +0700

    Add valid device is mobile or desktop

--- src/Modules/User/Components/Login/FacebookLogin/FacebookLogin.tsx
+++ src/Modules/User/Components/Login/FacebookLogin/FacebookLogin.tsx
@@ -1,10 +1,31 @@
 import * as React from 'react';
 import { Component } from 'react';
 
-export class FacebookLogin extends Component {
-  public render(){
-    return (
-      <div>this is facebook login</div>
-    )
+interface IFacebookLoginProps {
+  isMobile?: boolean;
+}
+
+const getIsMobile = () => {
+  let isMobile = false;
+
+  try {
+    isMobile = !!(
+      navigator.userAgent.match('CriOS') || navigator.userAgent.match(/mobile/i)
+    );
+  } catch (e) {
+    // TODO: remove it
+    console.log(e);
+  }
+
+  return isMobile;
+};
+
+export class FacebookLogin extends Component<IFacebookLoginProps, any> {
+  public static defaultProps = {
+    isMobile: getIsMobile(),
+  };
+
+  public render() {
+    return <div>this is facebook login</div>;
   }
 }
