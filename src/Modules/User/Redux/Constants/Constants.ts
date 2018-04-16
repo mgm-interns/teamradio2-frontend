d7e541ecf83c2d61c2a477f8b768e5e6db2ab1b3
Nam <namhoang.td09@gmail.com>
2018-04-16 20:45:14 +0700

    Update actionTypes

--- src/Modules/User/Redux/Constants/Constants.ts
+++ src/Modules/User/Redux/Constants/Constants.ts
@@ -1,27 +1,3 @@
-<<<<<<< HEAD
-import { actionTypes } from 'Modules/User/Redux/Constants';
-import { Reducer } from 'redux';
-import { IUser } from '../Types';
-
-const userInitialState: IUser = {
-  userInfo: null,
-  error: '',
-  loading: false,
-  isAuthenticated: false,
-};
-
-export const userReducer: Reducer<IUser> = (
-  state: IUser = userInitialState,
-  action,
-) => {
-  switch (action.type) {
-    case actionTypes.UPDATE_USER_PROFILE:
-      return { ...state, userInfo: action.payload };
-    default:
-      return state;
-  }
-=======
 export const actionTypes = {
   UPDATE_USER_PROFILE: 'UPDATE_USER_PROFILE',
->>>>>>> Update user avatar in header
 };
