b56a87449ceddf5a351fe791aeef9bf3c2e2d4d8
Nhac Tat Nguyen <nhac.tat.nguyen@mgm-tp.com>
2018-04-18 15:22:56 +0700

    Update help section v1.4

--- src/Pages/Help/HelpResource.tsx
+++ src/Pages/Help/HelpResource.tsx
@@ -9,21 +9,23 @@ A playlist for teams that can be edited collaboratively by all users
 ---
 ## Global score - (reputation): 
 + How to get point of reputation? You can get point from: 
-    - Updating your profile : The first time you update your profile, you can get point.
-      *  Avatar:  20 points
-      *  Other: 2 points/ field ( City, country, Bio, first name, last name)
+    - Updating your profile: The first time you update your profile, you can get points.
+      * Avatar: twenty points.
+      * Cover photo: twenty points.
+      * Other: two points/field and 10 points in total (city, country, bio, first name, and last name).
 
-    -	Creating your station: For each your station, you can get point 
-      * User: Each 2 users who joined your station you get 1 point. Each user is counted one time for the first times join your station.
-      * Song: Each (n) songs which was added to your station by another user you get (n) point.
-  
-    Note: **Point which you get from each station will be removed when you delete your station**
+    -	Creating your station: For each your station, you can get points. 
+      * User: Each two users who joined your station at the first time, you get one point.
+      * Song: Each song which is added to your station by other users, you get one point.
+      * Your songs are liked: For each up vote/like your songs receive from other users, you get one point.
+      * Your songs are disliked: there will be no minus points in the reputation of users.
+
 
 + What can you do with your point? 
-    - Increase number of your station: After you create your account, you have 1 station in limit. Each 30 points you have you can get 1 station in limit. More point you have more station you can create
-      * Example: *After you update full your profile, you can get 30 points and you have 2 stations in limit*
+    * Increase number of your private stations: After you create your new private account, you have one station in limit. Each thirty points you have you can get one more private station in limit. More points you have more stations you can create.
+      * Example: *After you update full your profile, you can get 50 points and then you have 1 station in limit*.
 
-    - Increase your point in station:  When the first times you join each station (not yours) you get a station score. By default as a new user, you have zero. Each 10% your reputation you get 1 station score.
+    - Increase your point in station: When the first times you join each station (not yours) you get a station score. By default as a new user, you have zero. Each 10% your reputation you get 1 station score.
     
       * Example: *After you update full your profile, you can get 30 points and you have 3 station scores*
 ---     
