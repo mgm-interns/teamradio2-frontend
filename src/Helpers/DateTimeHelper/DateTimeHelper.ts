bada3df6121e3965b963d9dd3da827d7e00877d7
Hau Tran <hatran@MGM-EDV.DE>
2018-04-18 14:01:06 +0700

    get list favorite for profile

--- src/Helpers/DateTimeHelper/DateTimeHelper.ts
+++ src/Helpers/DateTimeHelper/DateTimeHelper.ts
@@ -4,3 +4,19 @@ export const convertToEpochTimeInMillisSeconds = (time: number): number => {
 export const convertToEpochTimeInSeconds = (time: number): number => {
   return time / 1000;
 };
+
+export const getFormatedTime = (duration: number): string => {
+  const seconds = Math.floor((duration/1000)%60);
+  const minutes = Math.floor((duration/(1000*60))%60);
+  const hours = Math.floor((duration/(1000*60*60)));
+
+  const standard_seconds = (seconds < 10) ? "0" + seconds : seconds;
+
+  if (hours !== 0) {
+    const standard_minutes = (minutes < 10) ? "0" + minutes : minutes;
+    return hours + ":" + standard_minutes + ":" + standard_seconds
+  }
+
+  return minutes + ":" + standard_seconds
+};
+
---
+++ src/Modules/User/Components/Profile/ProfileBodyTabs/Favorite/FavoriteItem/FavoriteItem.tsx
@@ -0,0 +1,68 @@
+import { getFormatedTime } from 'Helpers/DateTimeHelper';
+import { Song } from 'Models/Song';
+import { Component } from 'react';
+import * as React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
+import './FavoriteItem.scss';
+
+export interface IFavoriteItem {
+  userId: string;
+  songId: string;
+  song: Song;
+}
+
+interface IFavoriteItemStates {
+  isShowTrash: boolean;
+}
+
+class FSPItem extends Component<
+  IFavoriteItem & RouteComponentProps<any>,
+  IFavoriteItemStates
+> {
+  constructor(props: IFavoriteItem & RouteComponentProps<any>) {
+    super(props);
+    this.handleMouseHover = this.handleMouseHover.bind(this);
+    this.state = {
+      isShowTrash: false,
+    };
+  }
+
+  public handleMouseHover() {
+    this.setState(this.toggleHoverState);
+  }
+
+  public toggleHoverState() {
+    return {
+      isShowTrash: !this.state.isShowTrash,
+    };
+  }
+
+  public render() {
+    const { userId, songId, song } = this.props;
+    console.log(song.duration);
+    return (
+      <div
+        className="favorite-song-item my-flex-item"
+        onMouseEnter={this.handleMouseHover}
+        onMouseLeave={this.handleMouseHover}>
+        {this.state.isShowTrash && (
+          <div className="trash-favorite-song">
+            <a href="#">
+              <span className="w3-jumbo w3-teal ">
+                <i className="fa fa-trash trash-size" />
+              </span>
+            </a>
+          </div>
+        )}
+        <div className="img-transition" />
+        <div className="duration">{getFormatedTime(song.duration)}</div>
+        <div className="favorite-song-thumbnail">
+          <img src={song.thumbnail} />
+        </div>
+        <div className="favorite-song-name">{song.title}</div>
+      </div>
+    );
+  }
+}
+
+export const FavoriteItem = withRouter(FSPItem);
