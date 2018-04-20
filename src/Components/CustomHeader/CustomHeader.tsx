5778b212fbc3a9a1027e6a33357ef0a22e591fbc
Tri Pham <pvtri96@gmail.com>
2018-04-20 16:46:49 +0700

    Fix header font style and use router link instead of tag a in header (#177)

--- src/Components/CustomHeader/CustomHeader.tsx
+++ src/Components/CustomHeader/CustomHeader.tsx
@@ -1,6 +1,7 @@
 import { UserDropdown } from 'Modules/User';
 import * as React from 'react';
 import { Component } from 'react';
+import { Link } from 'react-router-dom';
 import './CustomHeader.scss';
 
 const HEADER_MARGIN: number = 200;
@@ -17,8 +18,6 @@ export class CustomHeader extends Component<any, any> {
   public componentDidMount() {
     window.addEventListener('scroll', this.handleScroll);
   }
-
-
 
   public componentWillUnmount() {
     window.removeEventListener('scroll', this.handleScroll);
@@ -41,9 +40,9 @@ export class CustomHeader extends Component<any, any> {
         <div className="container">
           <div className="header-left">
             <div className="logo">
-              <a href="/">
+              <Link to="/">
                 <img alt="logo" src="/img/logo.png" />
-              </a>
+              </Link>
             </div>
           </div>
           <div className="header-right">
