07e29603157c90c708d7a270d94fee38e9853723
Nhac Tat Nguyen <nguyentatnhac@gmail.com>
2018-04-24 15:06:43 +0700

    Prevent unnecessary call history api (#195)

--- src/Modules/Station/Components/PlaylistTabs/PlaylistTabs.tsx
+++ src/Modules/Station/Components/PlaylistTabs/PlaylistTabs.tsx
@@ -111,7 +111,7 @@ export class PlaylistTabsComponent extends BaseComponent<IProps, IStates> {
             <Playlist playlist={playlist} stationId={stationId} />
           </TabPane>
           <TabPane tabId={HISTORY_TAB_ID}>
-            <History stationId={stationId} />
+            <History stationId={stationId} isActive={this.state.activeTab === HISTORY_TAB_ID} />
           </TabPane>
           <TabPane tabId={FAVOURITE_TAB_ID}>
             <Favorite />
