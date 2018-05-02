ae099eb9191929583713384222cbbf0a6140d583
mbnguyen <minhnb89@gmail.com>
2018-05-02 16:15:28 +0700

    Reconfig webpack to get env - use dotenv instead of dotenv-webpack

---
+++ config/getEnvironment.js
@@ -0,0 +1,27 @@
+const REACT_APP = /^REACT_APP_/i;
+
+require('dotenv').config();
+
+function getClientEnvironment() {
+  const raw = Object.keys(process.env)
+    .filter(key => REACT_APP.test(key))
+    .reduce(
+      (env, key) => {
+        env[key] = process.env[key];
+        return env;
+      },
+      {
+        NODE_ENV: process.env.NODE_ENV || 'development'
+      }
+    );
+  const stringified = {
+    'process.env': Object.keys(raw).reduce((env, key) => {
+      env[key] = JSON.stringify(raw[key]);
+      return env;
+    }, {}),
+  };
+
+  return {raw, stringified};
+}
+
+module.exports = getClientEnvironment;
