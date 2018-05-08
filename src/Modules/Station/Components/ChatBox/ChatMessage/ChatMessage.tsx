16da0be21e1303f5db252f461b8a5535a2e2ece4
mbnguyen <minhnb89@gmail.com>
2018-05-08 17:41:39 +0700

    Show default avatar for chat feature in case user has no avatar

--- src/Modules/Station/Components/ChatBox/ChatMessage/ChatMessage.tsx
+++ src/Modules/Station/Components/ChatBox/ChatMessage/ChatMessage.tsx
@@ -1,3 +1,4 @@
+import { DEFAULT_USER_AVATAR } from 'Modules/User';
 import { Component } from 'react';
 import * as React from 'react';
 import './ChatMessage.scss';
@@ -14,13 +15,24 @@ export class ChatMessage extends Component<IChatMessageProps, {}> {
     super(props);
   }
 
+  public getUserAvatar(avatarURL: string): string {
+    // TODO: client has to handle it because server return avatarUrl: "null". Will be updated when backend fix this bug
+    if (avatarURL && avatarURL !== 'null') {
+      return avatarURL;
+    }
+    return DEFAULT_USER_AVATAR;
+  }
+
   public render() {
     const { isOfCurrentUser, avatarUrl, userName, message } = this.props;
     return (
       <div className="chat-item-container">
         <div className={isOfCurrentUser ? 'message-right' : 'message-left'}>
           <div className={isOfCurrentUser ? 'display-none' : ''}>
-            <img src={avatarUrl} className="rounded-circle avatar" />
+            <img
+              src={this.getUserAvatar(avatarUrl)}
+              className="rounded-circle avatar"
+            />
           </div>
           <div className="message-container">
             <div className={isOfCurrentUser ? 'display-none' : 'username'}>
