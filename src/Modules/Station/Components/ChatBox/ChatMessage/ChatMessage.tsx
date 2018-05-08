21251c2ea0d4e0ce53e19c10678c4bda07728c2e
dungle1811 <dungle1811@gmail.com>
2018-05-08 17:28:50 +0700

    Fix problem that a long message is overflow and set default avatar for a user

--- src/Modules/Station/Components/ChatBox/ChatMessage/ChatMessage.tsx
+++ src/Modules/Station/Components/ChatBox/ChatMessage/ChatMessage.tsx
@@ -1,3 +1,4 @@
+import { DEFAULT_USER_AVATAR } from 'Modules/User/Constants';
 import { Component } from 'react';
 import * as React from 'react';
 import './ChatMessage.scss';
@@ -20,7 +21,12 @@ export class ChatMessage extends Component<IChatMessageProps, {}> {
       <div className="chat-item-container">
         <div className={isOfCurrentUser ? 'message-right' : 'message-left'}>
           <div className={isOfCurrentUser ? 'display-none' : ''}>
-            <img src={avatarUrl} className="rounded-circle avatar" />
+            <img
+              src={
+                this.isValidAvatar(avatarUrl) ? avatarUrl : DEFAULT_USER_AVATAR
+              }
+              className="rounded-circle avatar"
+            />
           </div>
           <div className="message-container">
             <div className={isOfCurrentUser ? 'display-none' : 'username'}>
@@ -32,4 +38,17 @@ export class ChatMessage extends Component<IChatMessageProps, {}> {
       </div>
     );
   }
+
+  private isValidAvatar = (avatarUrl: string) => {
+    switch (avatarUrl) {
+      case '':
+        return false;
+      case 'null':
+        return false;
+      case 'undefined':
+        return false;
+      default:
+        return true;
+    }
+  };
 }
