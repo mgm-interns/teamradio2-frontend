41a3203b5fd74b94f7deb420b8b18c86991e5eb2
Khanh Ly <baokhanh7m@gmail.com>
2018-05-11 11:42:09 +0700

    Add transform text and list anonymous users - Add reduce character name display on tooltip and popover - Add handle anonymous return from API

--- src/Modules/Station/Components/OnlineUsers/OnlineUsers.tsx
+++ src/Modules/Station/Components/OnlineUsers/OnlineUsers.tsx
@@ -24,6 +24,8 @@ interface IState {
   popoverOpen: boolean;
 }
 
+const LIST_USERS_SHOW = 8;
+
 export class OnlineUsersComponent extends BaseComponent<IProps, IState> {
   constructor(props: IProps) {
     super(props);
@@ -46,27 +48,36 @@ export class OnlineUsersComponent extends BaseComponent<IProps, IState> {
     return userInfo !== null;
   };
 
-  public isMe = (username?: string) => {
-    if (username && this.props.userInfo) {
-      return this.props.userInfo.username === username;
+  // TODO: using username instead id when backend support it
+  public isMe = (id: string) => {
+    const { userInfo: currentUser } = this.props;
+    if (id && currentUser) {
+      return currentUser.id === id;
     }
     return false;
   };
 
-  public renderPopoverContent(onlineUsers: RegisteredUser) {
-    const filteredListToArray = this.toArray(onlineUsers);
-    const { userInfo } = this.props;
+  public renderPopoverContent(onlineUsers: any[], numberOnline: 0) {
+    const { userInfo: currentUser } = this.props;
 
-    const filteredUsers = filteredListToArray.sort(
-      user => (user.username === userInfo.username ? 0 : 1),
+    const filteredUsers = onlineUsers.sort(
+      user => (user.username === currentUser.username ? 1 : 0),
     );
+
+    // Only display keep top 10 users
+    const filteredList = filteredUsers.filter(
+      (user, index) => index < LIST_USERS_SHOW,
+    );
+    // Group the rest people to anonymous group
+    const anonymousUsersCount = numberOnline - filteredList.length;
+
     return (
       <ListGroup className="popover-container">
-        {filteredUsers.map(
+        {filteredList.map(
           ({ id, name, username, avatarUrl, points }, index) => (
             <Link to={`/profile/${id}`} key={index}>
               <ListGroupItem
-                active={this.isMe(username)}
+                active={this.isMe(id)}
                 className="online-users-list-item">
                 <div className="online-users-shape">
                   <img
@@ -76,15 +87,57 @@ export class OnlineUsersComponent extends BaseComponent<IProps, IState> {
                   />
                 </div>
                 <span className="online-users-caption">
-                  {this.isUserInfoAvailable(currentUser) && this.isMe(username)
-                    ? `You (${points})`
-                    : `${name} (${points})`}
+                  {this.isUserInfoAvailable(currentUser) && this.isMe(id)
+                    ? `You (${points || 0})`
+                    : `${this.reduceByCharacters(name) ||
+                        'Unknown'} (${points || 0})`}
                 </span>
               </ListGroupItem>
             </Link>
           ),
         )}
+        {anonymousUsersCount > 0 ? (
+          <ListGroupItem className="online-users-list-item">
+            <div className="online-users-shape">
+              <span className="online-users-image">{anonymousUsersCount}</span>
+            </div>
+            <span className="online-users-caption">Anonymous</span>
+          </ListGroupItem>
+        ) : null}
       </ListGroup>
+    );
+  }
+
+  public renderOnlineTooltip(list: any[], numberOnline: 0, target: string) {
+    const { userInfo: currentUser } = this.props;
+
+    // Only display keep top 10 users
+    const filteredList = list.filter((user, index) => index < LIST_USERS_SHOW);
+    // Group the rest people to anonymous group
+    const anonymousUsersCount = numberOnline - filteredList.length;
+
+    if (this.state.popoverOpen) return null;
+    return (
+      <UncontrolledTooltip
+        placement="bottom"
+        target={target}
+        className="online-tooltip">
+        {filteredList.map(({ id, name, username }, index) => (
+          <div key={index} className="align-text-left">
+            <span>
+              {this.isUserInfoAvailable(currentUser) && this.isMe(id)
+                ? 'You'
+                : `${this.reduceByCharacters(name) || 'Unknown'}`}
+            </span>
+            <br />
+          </div>
+        ))}
+        {anonymousUsersCount > 0 ? (
+          <span className="align-text-left">
+            and {anonymousUsersCount} more
+          </span>
+        ) : null}
+      </UncontrolledTooltip>
     );
   }
 
@@ -94,6 +147,11 @@ export class OnlineUsersComponent extends BaseComponent<IProps, IState> {
     }
 
     const { onlineUsers, numberOnline } = this.props.data;
+
+    const filteredListToArray = this.toArray(onlineUsers);
+    const filteredListWithoutAnonymous = this.removeAnonymousFromArray(
+      filteredListToArray,
+    );
 
     return [
       <div
@@ -111,17 +169,33 @@ export class OnlineUsersComponent extends BaseComponent<IProps, IState> {
           {numberOnline || '0'} online
         </span>
       </div>,
-      <div key={3}>{this.renderOnlineTooltip(fixture, 'online-users')}</div>,
+      <div key={3}>
+        {this.renderOnlineTooltip(
+          filteredListWithoutAnonymous,
+          numberOnline,
+          'online-users',
+        )}
+      </div>,
       <Popover
         key={2}
         placement="bottom"
         isOpen={this.state.popoverOpen}
         toggle={this.toggle}
         target="online-users">
-        {this.renderPopoverContent(onlineUsers)}
+        {this.renderPopoverContent(filteredListWithoutAnonymous, numberOnline)}
       </Popover>,
     ];
   }
+
+  private reduceByCharacters = (text: string, length = 22) => {
+    let output = '';
+    for (let i = 0; i < text.length; i++) {
+      if (i < length - 1) {
+        output += text[i];
+      }
+    }
+    return output + (text.length > length - 1 ? '...' : '');
+  };
 
   private toArray = (userMap: any) => {
     return Object.keys(userMap).reduce((prev, key) => {
@@ -129,23 +203,19 @@ export class OnlineUsersComponent extends BaseComponent<IProps, IState> {
     }, []);
   };
 
-  private renderOnlineTooltip(list: any[], target: string) {
-    if (this.state.popoverOpen) return null;
-    return (
-      <UncontrolledTooltip placement="bottom" target={target}>
-        {list.map(({ name, username }, index) => (
-          <div className="online-tooltip" key={index}>
-            <span>
-              {this.isUserInfoAvailable(currentUser) && this.isMe(username)
-                ? 'You'
-                : name}
-            </span>
-            <br />
-          </div>
-        ))}
-      </UncontrolledTooltip>
-    );
-  }
+  private removeAnonymousFromArray = (list: any[]) => {
+    const filteredListWithoutAnonymous: any[] = [];
+    let countOnline = 0;
+
+    list.forEach(user => {
+      countOnline++;
+      if (user.username !== 'Anonymous') {
+        filteredListWithoutAnonymous.push(user);
+      }
+    });
+
+    return filteredListWithoutAnonymous;
+  };
 }
 
 const mapStateToProps = (state: IApplicationState) => ({
