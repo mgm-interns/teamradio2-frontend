fcd2a07d0c26b71279cc258cb0871ceb1e6c4579
Nhac Tat Nguyen <nhac.tat.nguyen@mgm-tp.com>
2018-05-17 15:18:31 +0700

    Cancel all subscription created when Profile page unmount\

--- src/Modules/User/Components/Profile/MyStationsBrowser/MyStationsBrowser.tsx
+++ src/Modules/User/Components/Profile/MyStationsBrowser/MyStationsBrowser.tsx
@@ -2,13 +2,19 @@ import { Inject } from 'Configuration/DependencyInjection';
 import { StationInfo } from 'Models';
 import { BaseStationBrowser } from 'Modules/Station';
 import * as React from 'react';
+import { Subscription } from 'rxjs/Subscription';
 import { UserServices } from 'Services/Http';
 
 export class MyStationsBrowser extends BaseStationBrowser<{}> {
   @Inject('UserServices') private userServices: UserServices;
+  private getListMyStationSub: Subscription;
 
   public componentWillMount() {
     this.getListStation();
+  }
+
+  public componentWillUnmount() {
+    this.cancelSubscription();
   }
 
   public getListStation() {
@@ -31,5 +37,11 @@ export class MyStationsBrowser extends BaseStationBrowser<{}> {
 
   public getNoStationFoundMessage() {
     return 'You have no station';
+  }
+
+  private cancelSubscription() {
+    if (this.getListMyStationSub) {
+      this.getListMyStationSub.unsubscribe();
+    }
   }
 }
--- src/Modules/User/Components/Profile/RecentStationsBrowser/UserRecentStationsBrowser.tsx
+++ src/Modules/User/Components/Profile/RecentStationsBrowser/UserRecentStationsBrowser.tsx
@@ -2,6 +2,7 @@ import { Inject } from 'Configuration/DependencyInjection';
 import { RegisteredUser } from 'Models/User';
 import { BaseStationBrowser } from 'Modules/Station';
 import * as React from 'react';
+import { Subscription } from 'rxjs/Subscription';
 import { UserServices } from 'Services/Http';
 
 interface IProps {
@@ -10,9 +11,14 @@ interface IProps {
 
 export class UserRecentStationsBrowser extends BaseStationBrowser<IProps> {
   @Inject('UserServices') private userServices: UserServices;
+  private getUserRecentStationSub: Subscription;
 
   public componentWillMount() {
     this.getListStation();
+  }
+
+  public componentWillUnmount() {
+    this.cancelSubscription();
   }
 
   public getListStation() {
@@ -37,6 +43,12 @@ export class UserRecentStationsBrowser extends BaseStationBrowser<IProps> {
     const { userInfo } = this.props;
     if (userInfo) {
       return userInfo.name + " hasn't interact with any station yet";
+    }
+  }
+
+  private cancelSubscription() {
+    if (this.getUserRecentStationSub) {
+      this.getUserRecentStationSub.unsubscribe();
     }
   }
 }
