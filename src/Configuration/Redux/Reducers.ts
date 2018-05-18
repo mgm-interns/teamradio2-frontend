381cedac8becfe5fceceeecdbf6f56cc08a64af6
Khanh Ly <baokhanh7m@gmail.com>
2018-05-18 10:41:23 +0700

    Solve conflict

--- src/Configuration/Redux/Reducers.ts
+++ src/Configuration/Redux/Reducers.ts
@@ -1,9 +1,12 @@
 import { IStationsListState, stationsReducer } from 'Modules/Station/Redux';
-import { chatReducer, stationReducer } from 'Modules/Station/Redux/Reducer';
+import {
+  chatReducer,
+  stationReducer,
+  volumeReducer,
+} from 'Modules/Station/Redux/Reducer';
 import { IMessage, IStationState } from 'Modules/Station/Redux/Types';
-import { favoriteReducer, userReducer } from 'Modules/User/Redux/Reducer';
-import { volumeReducer } from 'Modules/Station/Redux/Reducer';
 import { IVolume } from 'Modules/Station/Redux/Types/IVolume';
+import { favoriteReducer, userReducer } from 'Modules/User/Redux/Reducer';
 import { IUser } from 'Modules/User/Redux/Types';
 import { IFavorite } from 'Modules/User/Redux/Types/Types';
 import { combineReducers, Reducer } from 'redux';
