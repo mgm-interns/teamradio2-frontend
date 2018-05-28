815e6b744a61728efe88f15dc5dae56928e7377c
Hau Tran <36954907+hau-tran@users.noreply.github.com>
2018-05-28 15:57:49 +0700

    Add accept only image type profile (#450)

--- src/Modules/User/Components/Profile/ImageUploader/ImageUploader.tsx
+++ src/Modules/User/Components/Profile/ImageUploader/ImageUploader.tsx
@@ -8,7 +8,11 @@ import Cropper from 'react-cropper';
 import { connect } from 'react-redux';
 import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
 import { UserServices } from 'Services/Http';
-import { MAXIMUM_IMAGE_TO_BYTE, MAXIMUM_IMAGE_TO_MB } from '../../../Constants';
+import {
+  ACCEPTED_FILE_TYPE,
+  MAXIMUM_IMAGE_TO_BYTE,
+  MAXIMUM_IMAGE_TO_MB,
+} from '../../../Constants';
 import { updateUserInfo } from '../../../Redux/Actions';
 import './ImageUploader.scss';
 
@@ -126,6 +130,9 @@ class ImageUploaderComponent extends BaseComponent<IProps, IState> {
       this.showError(
         `The picture size can not exceed ${MAXIMUM_IMAGE_TO_MB}MB.`,
       );
+    } else if (uploadedImage.type.split('/')[0] !== ACCEPTED_FILE_TYPE) {
+      this.setAllValueToDefault();
+      this.showError(`Only ${ACCEPTED_FILE_TYPE} types are accepted`);
     } else {
       const base64 = await fileContentToBase64(uploadedImage);
       await this.setStateAsync({ uploadedImage: base64 });
