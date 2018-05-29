d25eab476f3d6ccb2b24dd901f8593c49577067a
Nhac Tat Nguyen <nhac.tat.nguyen@mgm-tp.com>
2018-05-29 09:54:57 +0700

    Use link for station browser item

--- src/Modules/Station/Components/StationBrowser/StationBrowserItem/StationBrowserItem.tsx
+++ src/Modules/Station/Components/StationBrowser/StationBrowserItem/StationBrowserItem.tsx
@@ -2,50 +2,50 @@ import * as classNames from 'classnames';
 import { StationInfo } from 'Models';
 import { Component } from 'react';
 import * as React from 'react';
-import { RouteComponentProps, withRouter } from 'react-router-dom';
+import { Link, RouteComponentProps, withRouter } from 'react-router-dom';
 import { UncontrolledTooltip } from 'reactstrap';
 import './StationBrowserItem.scss';
 
 type IProps = StationInfo & RouteComponentProps<any>;
 
 class SBItem extends Component<IProps, {}> {
-  public joinStation = () => {
-    const { friendlyId } = this.props;
-    this.props.history.push(`/station/${friendlyId}`);
-  };
-
   public render() {
     const { name, numberOnline = 0, picture, id } = this.props;
     return (
-      <div className="station-item d-flex" onClick={this.joinStation}>
-        <div className="thumbnail">
-          {picture && (
-            <div className="sound-wave">
-              <span />
-              <span />
-              <span />
-              <span />
-              <span />
+      <Link to={`/station/${this.props.friendlyId}`}>
+        <div className="station-item d-flex">
+          <div className="thumbnail">
+            {picture && (
+              <div className="sound-wave">
+                <span />
+                <span />
+                <span />
+                <span />
+                <span />
+              </div>
+            )}
+            <img src={picture || '/img/station_default_cover.png'} />
+            <div className="online-users">
+              <i
+                className={classNames('fa', {
+                  'fa-circle': numberOnline > 0,
+                  'fa-circle-o': numberOnline <= 0,
+                })}
+              />
+              <span> {numberOnline} online</span>
             </div>
-          )}
-          <img src={picture || '/img/station_default_cover.png'} />
-          <div className="online-users">
-            <i
-              className={classNames('fa', {
-                'fa-circle': numberOnline > 0,
-                'fa-circle-o': numberOnline <= 0,
-              })}
-            />
-            <span> {numberOnline} online</span>
+          </div>
+          <div className="station-name">
+            <span id={`station-` + id}>{name}</span>
+            <UncontrolledTooltip
+              placement="top"
+              target={`station-` + id}
+              delay={0}>
+              {name}
+            </UncontrolledTooltip>
           </div>
         </div>
-        <div className="station-name">
-          <span id={`station-` + id}>{name}</span>
-          <UncontrolledTooltip placement="top" target={`station-` + id}>
-            {name}
-          </UncontrolledTooltip>
-        </div>
-      </div>
+      </Link>
     );
   }
 }
